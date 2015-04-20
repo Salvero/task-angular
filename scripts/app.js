1540b9861380c799470fb58074f44e7888f828d1
Salman Akram <thunder9ruby@gmail.com>
2015-04-20 17:02:46 -0400

    first commit for new Task App

---
+++ scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var app = angular
+  .module('TaskNinjaApp', [
+    'ngAnimate',
+    'ngResource',    
+    'ngRoute',    
+    'firebase'
+  ])
+  .constant('FURL', 'https://your-firebase.firebaseio.com/')  
+  .config(function ($routeProvider) {
+    $routeProvider      
+      .when('/', {
+        templateUrl: 'views/main.html'        
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
