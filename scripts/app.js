700d3376f816389c7f34c04e59f609bccd5e1e5f
Salman Akram <thunder9ruby@gmail.com>
2015-04-20 18:15:52 -0400

    create posting and browsing task

--- scripts/app.js
+++ scripts/app.js
@@ -7,12 +7,24 @@ var app = angular
     'ngRoute',    
     'firebase'
   ])
-  .constant('FURL', 'https://your-firebase.firebaseio.com/')  
+  .constant('FURL', 'https://task-angular-app.firebaseio.com/')  
   .config(function ($routeProvider) {
     $routeProvider      
       .when('/', {
         templateUrl: 'views/main.html'        
       })
+      .when('/post', {
+        templateUrl: 'views/post.html',
+        controller: 'TaskController'
+      })
+      .when('/edit/:taskId', {
+        templateUrl: 'views/edit.html',
+        controller: 'TaskController'
+      })
+      .when('/browse', {
+        templateUrl: 'views/browse.html',
+        controller: 'TaskController'
+      })            
       .otherwise({
         redirectTo: '/'
       });
